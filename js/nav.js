f0ebaf7b38319be2fa7dc2b8f80d22e63246d88c
ravindranag <ravindranag52@gmail.com>
2021-10-15 19:21:33 +0530

    home section

--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,71 @@
 const open = document.querySelector('#nav-menu-open');
-const close = document.querySelector('#nav-menu-close');
+const close = document.querySelector('.close-overlay');
 const menu = document.querySelector('.nav-menu');
+const mobileNav = () => {
+    menu.classList.toggle('nav-menu--show');
+    close.classList.toggle('display');
+}
 
+open.addEventListener('click', mobileNav);
 
-open.addEventListener('click', () => {
-    open.classList.toggle('nav-show');
-    close.classList.toggle('nav-show');
-    open.style.pointerEvents = 'none';
-    close.style.pointerEvents = 'auto';
-    menu.style.right = '-190px';
-    console.log('open');
+close.addEventListener('click', mobileNav);
+
+const navLogo = document.querySelector('.nav-logo');
+const exploreBtn = document.querySelector('.home-explore');
+const about = document.querySelector('#home > nav > div.nav-menu > span:nth-child(1)');
+const event = document.querySelector('#home > nav > div.nav-menu > span:nth-child(2)');
+const wing = document.querySelector('#home > nav > div.nav-menu > span:nth-child(3)');
+const startup = document.querySelector('#home > nav > div.nav-menu > span:nth-child(4)');
+const gallery = document.querySelector('#home > nav > div.nav-menu > span:nth-child(5)');
+const testimonial = document.querySelector('#home > nav > div.nav-menu > span:nth-child(6)');
+const contact = document.querySelector('#home > nav > div.nav-menu > span:nth-child(7)');
+const homeSection = document.querySelector('#home');
+const aboutSection = document.querySelector('#about');
+// const eventSection = document.querySelector('#events');
+const wingSection = document.querySelector('#wings');
+const startupSection = document.querySelector('#ourstar');
+const gallerySection = document.querySelector('#portfolio');
+const testSection = document.querySelector('#testimonials');
+const contactSection = document.querySelector('#contact-form');
+
+navLogo.addEventListener('click', () => {
+    homeSection.scrollIntoView();
+});
+about.addEventListener('click', () => {
+    aboutSection.scrollIntoView();
+});
+exploreBtn.addEventListener('click', () => {
+    aboutSection.scrollIntoView();
+});
+wing.addEventListener('click', () => {
+    wingSection.scrollIntoView();
+});
+startup.addEventListener('click', () => {
+    startupSection.scrollIntoView();
+});
+gallery.addEventListener('click', () => {
+    gallerySection.scrollIntoView();
+});
+testimonial.addEventListener('click', () => {
+    testSection.scrollIntoView();
+});
+contact.addEventListener('click', () => {
+    contactSection.scrollIntoView();
 });
 
-close.addEventListener('click', () => {
-    open.classList.toggle('nav-show');
-    close.classList.toggle('nav-show');
-    close.style.pointerEvents = 'none';
-    open.style.pointerEvents = 'auto';
-    menu.style.right = '20px';
-    console.log('close');
-});
+const homeOptions = {
+    rootMargin: "-70% 0px 0px 0px"
+};
+const navBar = document.querySelector('nav');
+const homeObserver = new IntersectionObserver((sections) => {
+    sections.forEach(section => {
+        // console.log(section.target);
+        if (!section.isIntersecting) {
+            navBar.classList.add('scrolled');
+        } else {
+            navBar.classList.remove('scrolled');
+        }
+    });
+}, homeOptions);
+
+homeObserver.observe(homeSection);
--- js/preloader.js
+++ js/preloader.js
@@ -4,20 +4,48 @@ const logoAside = document.querySelector('.logo-aside');
 const content = document.querySelector('.home-content');
 const gif = document.querySelector('.preload');
 const sections = document.querySelectorAll('section');
+const home = document.querySelector('#home');
+const background = document.querySelector('.background');
+const explore = document.querySelector('.home-explore');
 
-window.addEventListener('load', () => {
-    gif.style.opacity = '0';
-    gif.style.pointerEvents = 'none';
-    nav.style.opacity = '1';
-    nav.style.pointerEvents = 'auto';
-    content.style.opacity = '1';
-    content.style.transform = 'translateY(0)';
+const logoReveal = () => {
     logo.style.opacity = '1';
     logo.style.transform = 'translateX(0)';
     logoAside.style.opacity = '1';
     logoAside.style.transform = 'translateX(0)';
+};
+
+const contentReveal = () => {
+    content.style.opacity = '1';
+    content.style.transform = 'translateY(0)';
+}
+
+const navReveal = () => {
+    nav.style.opacity = '1';
+    nav.style.pointerEvents = 'auto';
+    explore.style.opacity = '1';
+    explore.classList.add('bounce-up');
+};
+
+const allowScroll = () => {
     document.body.style.overflow = 'auto';
     sections.forEach(section => {
         section.style.display = 'block';
-    })
+    });
+    home.classList.add('home-bg');
+};
+
+const bgShow = () => {
+    background.classList.add('background-show');
+};
+
+window.addEventListener('load', () => {
+    gif.style.opacity = '0';
+    gif.style.pointerEvents = 'none';
+    logoReveal();
+    setTimeout(contentReveal, 1000);
+    setTimeout(bgShow, 2000);
+    setTimeout(navReveal, 2500);
+    setTimeout(allowScroll, 3000);
+
 });
